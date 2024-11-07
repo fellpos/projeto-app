ca4e5212adaaa3eafac64da5a0b04e1426589a1b
fellpos <ra54399754893@acaonsfatima.org.br>
2024-11-07 18:13:02 -0300

    começando basico estilização

--- src/screens/drawer/index.tsx
+++ src/screens/drawer/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from 'react-native';
- 
+import { Text, View, StyleSheet } from 'react-native';
+
 export default function App() {
     return (
-        <View>
-            <Text> Hello World</Text>
+        <View style={styles.container}>
+            <Text style={styles.texto}> Hello World</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    texto: {
+        fontSize: 20,
+        color: 'red',
+    }
+})
